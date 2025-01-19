29e9010f3a33a5acaadae756f3b82b264d2d0067
Udit21Ag <150537148+Udit21Ag@users.noreply.github.com>
2025-01-19 17:32:23 +0530

    Added project files

---
+++ src/data/Dropdown.js
@@ -0,0 +1,41 @@
+export const AbtMenu = [
+	{
+		label: "ABOUT",
+		link: "https://www.veritasium.com/about",
+	},
+	{
+		label: "EDUCATION",
+		link: "https://www.veritasium.com/education-1",
+	},
+	{
+		label: "EMPLOYMENT",
+		link: "https://www.veritasium.com/employment",
+	},
+	{
+		label: "TALKS",
+		link: "https://www.veritasium.com/talks-1",
+	},
+	{
+		label: "AWARDS",
+		link: "https://www.veritasium.com/awards",
+	},
+	{
+		label: "MEDIA",
+		link: "https://www.veritasium.com/media",
+	},
+	{
+		label: "PUBLICATIONS",
+		link: "https://www.veritasium.com/publications",
+	},
+];
+
+export const PtrnMenu = [
+	{
+		label: "WALL OF THANKS",
+		link: "https://www.veritasium.com/wall-of-thanks",
+	},
+	{
+		label: "SUPPORT VERITASIUM",
+		link: "http://patreon.com/veritasium",
+	},
+];
